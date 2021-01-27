9b6585c22068db371903a905d65690f49a965b5a
Xin Guan <xin.guan@gmail.com>
2021-01-26 20:59:59 -0800

    added proxy and url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://safe-oasis-66111.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
